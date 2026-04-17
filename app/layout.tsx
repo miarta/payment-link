c93fa65e2427da5fe15acedeba25fd1bb5749052
miarta <miarta@ipaymu.com>
2026-04-17 17:34:04 +0800

    landing pages payment link v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>iPaymu - Payment Links</title>
+        <link rel="icon" href="/fav.ico" type="image/x-icon" />
       </head>
       <body className="antialiased">{children}</body>
     </html>
